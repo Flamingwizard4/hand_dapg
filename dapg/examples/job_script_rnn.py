32e17ba9d5aa434a7b2c1ee3d9e35b5054c0c185
Flamingwizard4 <flamingwizard4@gmail.com>
2021-11-18 11:50:55 -0500

    modified:   dapg/examples/bcrl.txt 	modified:   dapg/examples/job_script_rnn.py 	dapg/utils/bcrl.txt 	dapg/utils/visualize_policy_rnn_train.py

--- dapg/examples/job_script_rnn.py
+++ dapg/examples/job_script_rnn.py
@@ -49,6 +49,7 @@ with open(EXP_FILE, 'w') as f:
 # ===============================================================================
 
 e = GymEnv(job_data['env'])
+'''
 ###Varying Number of Layers
 init_layers = 2
 tested_layers = 4
@@ -129,8 +130,10 @@ plt.plot(range(2, 2+tested_layers), avg_test_rewards_n, label="Test Loss")
 plt.title("RNN BC Depth vs Train/Test Loss")
 plt.xlabel("# of Layers")
 plt.ylabel("Avg. Loss")
+plt.legend()
 plt.savefig("rnn_bc_%d-%dn_alone_graph.png"%(init_layers, init_layers + tested_layers))
 plt.close()
+'''
 '''
 ### Varying Batch Size
 init_batch = 1
@@ -216,11 +219,98 @@ plt.close()
 '''
 '''
 ### Varying Training Epochs
-init_epochs = 20
-tested_epochs = 3
+init_hz = 16
+tested_hz = 4
+avg_train_rewards_h, avg_test_rewards_h = [], []
+for h in [init_hz*(2**x) for x in range(tested_hz)]:
+    policy = RNN(e.spec, hidden_size=h, n_layers=job_data['bc_n_layers'], seed=job_data['seed'])
+    #policy = MLP(e.spec, hidden_sizes=job_data['policy_size'], seed=job_data['seed'])
+    #baseline = MLPBaseline(e.spec, reg_coef=1e-3, batch_size=job_data['vf_batch_size'],
+                        #epochs=job_data['vf_epochs'], learn_rate=job_data['vf_learn_rate'])
+
+    # Get demonstration data if necessary and behavior clone
+    if job_data['algorithm'] != 'NPG':
+        print("========================================")
+        print("Collecting expert demonstrations")
+        print("========================================")
+        demo_paths = pickle.load(open(job_data['demo_file'], 'rb'))
+        bc_agent = rnn_BC(demo_paths[:-5], policy, e, epochs=job_data['bc_epochs'], seed=job_data['seed'], batch_size=job_data['bc_batch_size'],
+                    lr=job_data['bc_learn_rate'], loss_type='MSE', set_transforms=False)
+        in_shift, in_scale, out_shift, out_scale = bc_agent.compute_transformations()
+        bc_agent.set_transformations(in_shift, in_scale, out_shift, out_scale)
+        bc_agent.set_variance_with_data(out_scale)
+
+        ts = timer.time()
+        print("========================================")
+        print("Running %d-hidden size, %d-layer, %d-batch size RNN BC with expert demonstrations for %d epochs"%(h, job_data['bc_n_layers'], job_data['bc_batch_size'], job_data['bc_epochs']))
+        print("========================================")
+        lox = bc_agent.train()
+        print("========================================")
+        print("BC training complete !!!")
+        print("time taken = %f" % (timer.time() - ts))
+        print("========================================")
+
+        # if job_data['eval_rollouts'] >= 1:
+        #     score = e.evaluate_train_vs_test(policy, demo_paths, num_episodes=job_data['eval_rollouts'], mean_action=True, seed=321)
+        #     print("Score with behavior cloning = %f" % score[0][0])
+        #     print("Performance with BC: %d / %d"%(score[0][4], job_data['eval_rollouts']))
+        train_losses, test_losses = e.evaluate_train_vs_test(bc_agent, demo_paths)
+        train_score, test_score, p = 0, 0, 0
+        for r in train_losses:
+            #print("Training loss with mse cloning = %f" % r[0])
+            #print("Score with selfrolled mse = %f" % r[1])
+            #print("Performance with selfrolled policy: %d" % r[2])
+            train_score += r[0]
+        for r in test_losses:
+            #print("Test loss with mse cloning = %f" % r[0])
+            #print("Score with selfrolled mse = %f" % r[1])
+            #print("Performance with selfrolled policy: %d" % r[2])
+            test_score += r[0] #traditional mse
+            p += r[2]
+        train_score /= len(train_losses)
+        test_score /= len(test_losses)
+        p /= len(test_losses)
+    with open("rnn_bc_%dhz_%dn_%db_%de_alone_log.txt"%(h, job_data['bc_n_layers'], job_data["bc_batch_size"], job_data['bc_epochs']), 'a') as log_file:
+        for lo in lox:
+            log_file.write("%f\n"%lo)
+        log_file.write("Training performance: %d / %d\n"%(p, len(test_losses)))#(score[0][4], job_data['eval_rollouts']))
+        log_file.write("Average training loss: %f\n"%train_score)
+        log_file.write("Average eval loss: %f"%test_score)
+    
+    plt.plot(lox)
+    plt.title("RNN BC Alone w/ Hidden Size of %d "%h)
+    plt.xlabel("Hidden Size")
+    plt.xscale('linear')
+    plt.ylabel("Batch Loss")
+    plt.legend()
+    plt.savefig("rnn_bc_%dhz_%dn_%db_%de_alone_log.png"%(h, job_data['bc_n_layers'], job_data["bc_batch_size"], job_data['bc_epochs']))
+    plt.close()
+    if job_data['algorithm'] != 'DAPG':
+        # We throw away the demo data when training from scratch or fine-tuning with RL without explicit augmentation
+        demo_paths = None
+
+    pickle.dump(policy, open('rnn_bc_%dhz_%dn_%db_%de_alone.pickle'%(h, job_data['bc_n_layers'], job_data["bc_batch_size"], job_data['bc_epochs']), 'wb'))
+    avg_train_rewards_h.append(train_score)
+    avg_test_rewards_h.append(test_score)
+    
+
+plt.plot([init_hz*(2**x) for x in range(tested_hz)], avg_train_rewards_h, label="Train Loss")
+plt.plot([init_hz*(2**x) for x in range(tested_hz)], avg_test_rewards_h, label="Test Loss")
+plt.title("RNN BC Hidden Size vs Test/Train Loss")
+plt.xlabel("Hidden Size")
+plt.xscale('log')
+plt.ylabel("Avg. Loss")
+plt.legend()
+plt.savefig("rnn_bc_%d-%dhz_alone_graph.png"%(init_hz, init_hz*2**(tested_hz-1)))
+plt.close()
+'''
+
+### Varying Training Epochs
+init_epochs = 240
+tested_epochs = 1
 avg_train_rewards_t, avg_test_rewards_t = [], []
 for t in [init_epochs*(2**x) for x in range(tested_epochs)]:
-    policy = RNN(e.spec, hidden_size=job_data['bc_hidden_size'], n_layers=job_data['bc_n_layers'], seed=job_data['seed'])
+    policy = RNN(e.spec, hidden_size=job_data['bc_hidden_size'], n_layers=job_data['bc_n_layers'], fc_sizes=tuple([job_data['bc_mlp_hz'] for f in range(2)]), seed=job_data['seed'])
     #policy = MLP(e.spec, hidden_sizes=job_data['policy_size'], seed=job_data['seed'])
     #baseline = MLPBaseline(e.spec, reg_coef=1e-3, batch_size=job_data['vf_batch_size'],
                         #epochs=job_data['vf_epochs'], learn_rate=job_data['vf_learn_rate'])
@@ -254,14 +344,14 @@ for t in [init_epochs*(2**x) for x in range(tested_epochs)]:
         train_losses, test_losses = e.evaluate_train_vs_test(bc_agent, demo_paths)
         train_score, test_score, p = 0, 0, 0
         for r in train_losses:
-            #print("Training loss with mse cloning = %f" % r[0])
-            #print("Score with selfrolled mse = %f" % r[1])
-            #print("Performance with selfrolled policy: %d" % r[2])
+            print("Training loss with mse cloning = %f" % r[0])
+            print("Score with selfrolled mse = %f" % r[1])
+            print("Performance with selfrolled policy: %d" % r[2])
             train_score += r[0]
         for r in test_losses:
-            #print("Test loss with mse cloning = %f" % r[0])
-            #print("Score with selfrolled mse = %f" % r[1])
-            #print("Performance with selfrolled policy: %d" % r[2])
+            print("Test loss with mse cloning = %f" % r[0])
+            print("Score with selfrolled mse = %f" % r[1])
+            print("Performance with selfrolled policy: %d" % r[2])
             test_score += r[0] #traditional mse
             p += r[2]
         train_score /= len(train_losses)
@@ -275,7 +365,7 @@ for t in [init_epochs*(2**x) for x in range(tested_epochs)]:
         log_file.write("Average eval loss: %f"%test_score)
     
     plt.plot(lox)
-    plt.title("RNN BC Alone w/ %d Epochs"%t)
+    plt.title("%d-layer, %d-batch size RNN BC Alone w/ %d Epochs"%(job_data['bc_n_layers'], job_data['bc_batch_size'], t))
     plt.xlabel("Epoch")
     plt.xscale('linear')
     plt.ylabel("Batch Loss")
@@ -296,9 +386,10 @@ plt.title("RNN BC Training Epochs vs Test/Train Loss")
 plt.xlabel("Training Epochs")
 plt.xscale('log')
 plt.ylabel("Avg. Loss")
+plt.legend()
 plt.savefig("rnn_bc_%d-%dt_alone_graph.png"%(init_epochs, init_epochs*2**(tested_epochs-1)))
 plt.close()
-'''
+
 '''
 ### Varying Learning Rates
 tested_rates = 5
@@ -378,6 +469,7 @@ plt.title("RNN BC LR vs Train/Test Loss")
 plt.xlabel("Learning Rate")
 plt.xscale('log')
 plt.ylabel("Avg. Loss")
+plt.legend()
 plt.savefig("rnn_bc_%f-%flr_alone_graph.png"%(0.0001, 0.0001*10**(tested_rates-1)))
 plt.close()
 '''
